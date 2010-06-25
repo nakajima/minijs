48f1969d98710f878ebeb3a8f9b1a6289cbdfc8c
Pat Nakajima <patnakajima@gmail.com>
2010-06-25 18:27:34 -0500

    Rebuilt minified version with new event support

--- minijs.js
+++ minijs.js
@@ -1,14 +1,16 @@
 
-(function(){function each(collection,callback){for(i in collection){var item=collection[i];callback.call(item,item);}}
-var proxy=function(elems){this.elems=elems;this.length=elems.length;}
-var methods={each:function(callback){each(this.elems,callback)
-return this;},css:function(styles){this.each(function(){for(prop in styles){this.style[prop]=styles[prop];}});},attr:function(){var prop=arguments[0],value=arguments[1];if(value){return this.each(function(){this.setAttribute(prop,value);});}else{return this.get().getAttribute(prop);}},removeAttr:function(prop){return this.each(function(){this.removeAttribute(prop);});},get:function(index){if(typeof index=="undefined"){return this.elems.length==1?this.elems[0]:this.elems;}else{return this.elems[index];}},size:function(){return this.elems.length;},prepend:function(content){return this.each(function(){this.innerHTML=content+this.innerHTML;});},append:function(content){return this.each(function(){this.innerHTML=this.innerHTML+content;});},html:function(content){if(content){return this.each(function(){this.innerHTML=content;});}else{return this.get(0).innerHTML;}},val:function(content){if(content){return this.each(function(){this.value=content;});}else{return this.get(0).value;}}}
-for(i in methods){proxy.prototype[i]=methods[i];}
-function $(css){var nodeSet=document.querySelectorAll(css);var nodes=[];var i=nodeSet.length;while(i--){nodes.push(nodeSet[i]);}
-return new proxy(nodes);}
-window.$=$;$.each=each;})();(function(){if(typeof $=='undefined'){throw("You must load base.js first!")}
+(function(){function each(collection,callback){for(i in collection){var item=collection[i];if(collection.hasOwnProperty(i)){callback.call(item);}}}
+var proxy=function(css,elems){this.selector=css;this.elems=elems;this.length=elems.length;}
+var methods={each:function(callback){this.elems.forEach(function(elem){callback.call(elem);});return this;},css:function(styles){return this.each(function(){for(prop in styles){this.style[prop]=styles[prop];}});},attr:function(){var prop=arguments[0],value=arguments[1];if(value){return this.each(function(){this.setAttribute(prop,value);});}else{return this.get().getAttribute(prop);}},removeAttr:function(prop){return this.each(function(){this.removeAttribute(prop);});},addClass:function(classNames){return this.each(function(){var classes=this.className.split(/\s+/);classes.push(classNames);this.className=classes.join(' ');});},removeClass:function(classNames){return this.each(function(){var classes=this.className.split(/\s+/);classNames=classNames.split(/\s+/);classNames.forEach(function(s){delete(classes[classes.indexOf(s)]);});this.className=classes.join(' ');});},is:function(selector){return this.get(0).webkitMatchesSelector(selector)},get:function(index){if(typeof index=="undefined"){return this.elems.length==1?this.elems[0]:this.elems;}else{return this.elems[index];}},toArray:function(){return this.elems;},size:function(){return this.elems.length;},prepend:function(content){return this.each(function(){this.innerHTML=content+this.innerHTML;});},append:function(content){return this.each(function(){this.innerHTML=this.innerHTML+content;});},html:function(content){if(content){return this.each(function(){this.innerHTML=content;});}else{return this.get(0).innerHTML;}},val:function(content){if(content){return this.each(function(){this.value=content;});}else{return this.get(0).value;}}}
+each(methods,function(){proxy.prototype[i]=methods[i];});function $(cssOrElem){if(typeof cssOrElem=='string'){var nodeSet=document.querySelectorAll(cssOrElem);var nodes=Array.prototype.slice.apply(nodeSet);}else{var nodes=[cssOrElem];}
+return new proxy(cssOrElem,nodes);}
+window.$=$;$.fn=proxy.prototype;$.each=each;})();(function(){if(typeof $=='undefined'){throw("You must load base.js first!")}
 function request(method,url,callback){var request=new XMLHttpRequest();request.open(method,url);request.onreadystatechange=function(){if(request.readyState==4){callback(request.responseText,request)}}
 request.send();}
 function get(url,callback){request('GET',url,callback);}
 function post(url,callback){request('POST',url,callback);}
-$.get=get;$.post=post;})();
+$.get=get;$.post=post;})();(function(){if(typeof $=='undefined'){throw("You must load base.js first!")}
+var delegators={};$.fn.on=function(eventName,handler){return this.each(function(){var elem=this;elem.addEventListener(eventName,function(event){if(handler.call($(elem),event)===false){event.preventDefault();}},true);});}
+$.fn.live=function(eventName,handler){$.delegate(eventName,this.selector,handler);}
+$.delegate=function(eventName,css,handler){if(!delegators[eventName]){delegators[eventName]=[];document.addEventListener(eventName,function(event){$.each(delegators[eventName],function(){this.call(this,event);});},false)}
+delegators[eventName].push(function(event){var elem=event.target;if($(elem).is(css)){if(handler.call($(elem),event)===false){event.preventDefault();}}});}})();
